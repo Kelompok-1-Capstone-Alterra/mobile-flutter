4b6a7e9f470debf5f2daf74be240b7fb4707f524
gus surya <odesuryawidnyana@gmail.com>
2023-05-25 15:55:35 +0800

    [FL-037] Slicing - Deskripsi Lengkap Feature

--- lib/view/toko/screen/deskripsi_produk.dart
+++ lib/view/toko/screen/deskripsi_produk.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_flutter/utils/themes/custom_color.dart';
-import 'package:mobile_flutter/models/toko_provider/base_model.dart';
-import 'package:mobile_flutter/utils/widget/toko_widget/reusable_button_chat.dart';
+import 'package:mobile_flutter/models/toko_model.dart';
+import 'package:mobile_flutter/view/toko/widget/toko_widget/reusable_button_chat.dart';
 import 'package:mobile_flutter/view/toko/screen/detail_produk.dart';
-import 'package:mobile_flutter/utils/widget/toko_widget/reusable_price.dart';
-import 'package:mobile_flutter/models/toko_provider/toko_data.dart';
+import 'package:mobile_flutter/view/toko/widget/toko_widget/reusable_price.dart';
+import 'package:mobile_flutter/view_model/toko_viewmodel/toko_data.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter_html/flutter_html.dart';
 
 class Deskripsi extends StatefulWidget {
   const Deskripsi({
@@ -49,10 +50,10 @@ class _DeskripsiState extends State<Deskripsi> {
                 "Deskripsi",
                 style: Theme.of(context).textTheme.headlineMedium,
               ),
-              Icon(
-                Icons.screen_share_outlined,
-                color: neutral[90],
-              ),
+              Image.asset(
+                'assets/images/toko_image/share.png',
+                color: neutral[100],
+              )
             ],
           ),
         ),
@@ -65,54 +66,37 @@ class _DeskripsiState extends State<Deskripsi> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  padding: const EdgeInsets.symmetric(horizontal: 5),
                   child: SizedBox(
                     width: size.width,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Row(
+                        SizedBox(
+                            child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Text(
-                              "Deskripsi Lengkap",
-                              style: Theme.of(context).textTheme.titleLarge,
+                            Expanded(
+                              child: Html(
+                                data: current.deskripsi,
+                              ),
                             ),
                           ],
-                        ),
-                        SizedBox(
-                          height: size.height * 0.020,
-                        ),
-                        SizedBox(
-                          height: size.height * 0.40,
-                          child: SingleChildScrollView(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Expanded(
-                                  child: Text(
-                                    current.deskripsi,
-                                    style:
-                                        Theme.of(context).textTheme.bodyMedium,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        )
+                        )),
                       ],
                     ),
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
                   child: SizedBox(
                     width: size.width,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         SizedBox(
-                          height: size.height * 0.020,
+                          height: size.height * 0.060,
                         ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -123,9 +107,6 @@ class _DeskripsiState extends State<Deskripsi> {
                             ),
                           ],
                         ),
-                        SizedBox(
-                          height: size.height * 0.016,
-                        ),
                       ],
                     ),
                   ),
@@ -134,122 +115,117 @@ class _DeskripsiState extends State<Deskripsi> {
                   padding:
                       const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                   width: size.width,
-                  height: size.height * 0.47,
                   child: GridView.builder(
-                    physics: const BouncingScrollPhysics(),
-                    itemCount: mainList.length,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 6 / 8,
-                      crossAxisSpacing: 15,
-                      mainAxisSpacing: 15,
-                    ),
-                    itemBuilder: (context, index) {
-                      BaseModel current = mainList[index];
-
-                      return GestureDetector(
-                        onTap: (() => Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) {
-                                FocusManager.instance.primaryFocus?.unfocus();
-                                return Details(
-                                  data: current,
-                                  isCameFromProduk: true,
-                                );
-                              }),
-                            )),
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 0),
-                          child: Card(
-                            margin: const EdgeInsets.all(0),
-                            elevation: 10,
-                            shadowColor: Colors.black26,
-                            surfaceTintColor: Colors.transparent,
-                            clipBehavior: Clip.antiAliasWithSaveLayer,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15.0),
-                            ),
-                            color: Colors.white,
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Expanded(
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                        image: AssetImage(current.imageUrl),
-                                        fit: BoxFit.cover,
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: mainList.length,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 6 / 8,
+                        crossAxisSpacing: 15,
+                        mainAxisSpacing: 15,
+                      ),
+                      itemBuilder: (context, index) {
+                        BaseModel current = mainList[index];
+                        return GestureDetector(
+                          onTap: (() => Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) {
+                                  FocusManager.instance.primaryFocus?.unfocus();
+                                  return Details(
+                                    data: current,
+                                    isCameFromProduk: true,
+                                  );
+                                }),
+                              )),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 0),
+                            child: Card(
+                              margin: const EdgeInsets.all(0),
+                              elevation: 10,
+                              shadowColor: Colors.black26,
+                              surfaceTintColor: Colors.transparent,
+                              clipBehavior: Clip.antiAliasWithSaveLayer,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                              color: Colors.white,
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                          image: AssetImage(current.imageUrl),
+                                          fit: BoxFit.cover,
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
-                                Expanded(
-                                  child: Align(
-                                    alignment: Alignment.topLeft,
-                                    child: Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                        vertical: 10,
-                                        horizontal: 10,
-                                      ),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          SizedBox(
-                                            width: MediaQuery.of(context)
-                                                    .size
-                                                    .width *
-                                                0.38,
-                                            child: AutoSizeText(
-                                              current.name,
-                                              overflow: TextOverflow.ellipsis,
-                                              minFontSize: 14,
-                                              maxLines: 2,
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .titleSmall,
-                                            ),
-                                          ),
-                                          SizedBox(
+                                  Expanded(
+                                    child: Align(
+                                      alignment: Alignment.topLeft,
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            SizedBox(
                                               width: MediaQuery.of(context)
                                                       .size
                                                       .width *
                                                   0.38,
-                                              child: ReuseablePrice(
-                                                price: current.price,
-                                              )),
-                                          SizedBox(
-                                            width: MediaQuery.of(context)
-                                                    .size
-                                                    .width *
-                                                0.38,
-                                            child: Padding(
-                                              padding:
-                                                  const EdgeInsets.symmetric(
-                                                      vertical: 5.0),
                                               child: AutoSizeText(
-                                                "${current.review.toString()}RB dilihat",
+                                                current.name,
+                                                overflow: TextOverflow.ellipsis,
+                                                minFontSize: 14,
+                                                maxLines: 2,
                                                 style: Theme.of(context)
                                                     .textTheme
-                                                    .bodySmall,
-                                                maxLines: 1,
+                                                    .titleSmall,
                                               ),
                                             ),
-                                          ),
-                                        ],
+                                            SizedBox(
+                                                width: MediaQuery.of(context)
+                                                        .size
+                                                        .width *
+                                                    0.38,
+                                                child: ReuseablePrice(
+                                                  price: current.price,
+                                                )),
+                                            SizedBox(
+                                              width: MediaQuery.of(context)
+                                                      .size
+                                                      .width *
+                                                  0.38,
+                                              child: Padding(
+                                                padding:
+                                                    const EdgeInsets.symmetric(
+                                                        vertical: 5.0),
+                                                child: AutoSizeText(
+                                                  "${current.review.toString()}RB dilihat",
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .bodySmall,
+                                                  maxLines: 1,
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      );
-                    },
-                  ),
+                        );
+                      }),
                 ),
                 Padding(
                   padding: EdgeInsets.only(top: size.height * 0.03),
