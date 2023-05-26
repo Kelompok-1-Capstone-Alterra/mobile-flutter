28c77418bdb72ed42a10401f47decc8b5c48e729
Haf1dzsr <hsramadhan68@gmail.com>
2023-05-26 13:44:29 +0700

    [FL-009] Fix About Screen

--- lib/view/settings/screens/about_screen.dart
+++ lib/view/settings/screens/about_screen.dart
@@ -15,7 +15,7 @@ class AboutScreen extends StatelessWidget {
           ),
         ),
         title: Text(
-          'About',
+          'Tentang Agriplan',
           style: ThemeData().textTheme.headlineSmall!.copyWith(
                 fontWeight: FontWeight.w600,
                 fontSize: 24,
@@ -29,16 +29,14 @@ class AboutScreen extends StatelessWidget {
         ),
         child: ListView(
           children: [
-            Expanded(
-              child: Text(
-                "Agriplan adalah solusi yang tepat untuk kegiatan menanammu. Agriplan memberikan rekomendasi-rekomendasi mengenai tanaman yang cocok untuk sekitarmu. Selain itu kami menyediakan berbagai artikel yang bisa kamu baca sebagai bahan referensi memulai aktivitas berkebun kamu. Dan juga kamu akan mendapatkan informasi yang bisa kamu lihat mulai dari: \n \n  \u2022   Cara menanam    \n  \u2022   Pemupukan  \n  \u2022   Penyiraman \n  \u2022   Sampai Temperatur Tanaman Ideal \n \nUntuk menunjang nutrisi dari tanamanmu, Agriplan bekerja sama dengan para penjual bahan-bahan serta alat pertanaian terpercaya. Jadi tidak perlu khawatir untuk memulai menanam. Anda juga bisa memiliki pengingat untuk penyiraman setiap harinya dan akan ada progress harian untuk memantau perkembangan tanaman Anda.",
-                style: ThemeData().textTheme.titleMedium!.copyWith(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 16,
-                      wordSpacing: 0.15,
-                    ),
-                textAlign: TextAlign.justify,
-              ),
+            Text(
+              "Agriplan adalah solusi yang tepat untuk kegiatan menanammu. Agriplan memberikan rekomendasi-rekomendasi mengenai tanaman yang cocok untuk sekitarmu. Selain itu kami menyediakan berbagai artikel yang bisa kamu baca sebagai bahan referensi memulai aktivitas berkebun kamu. Dan juga kamu akan mendapatkan informasi yang bisa kamu lihat mulai dari: \n \n  \u2022   Cara menanam    \n  \u2022   Pemupukan  \n  \u2022   Penyiraman \n  \u2022   Sampai Temperatur Tanaman Ideal \n \nUntuk menunjang nutrisi dari tanamanmu, Agriplan bekerja sama dengan para penjual bahan-bahan serta alat pertanaian terpercaya. Jadi tidak perlu khawatir untuk memulai menanam. Anda juga bisa memiliki pengingat untuk penyiraman setiap harinya dan akan ada progress harian untuk memantau perkembangan tanaman Anda.",
+              style: ThemeData().textTheme.titleMedium!.copyWith(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16,
+                    wordSpacing: 0.15,
+                  ),
+              textAlign: TextAlign.justify,
             ),
           ],
         ),
