e7eb313791bcae6c2e30729460c9f6f464be7ea1
gus surya <odesuryawidnyana@gmail.com>
2023-05-26 23:56:12 +0800

    [FL-037] Fix - Deskripsi Lengkap Feature (mengatur letak button chat penjual)

--- lib/view/toko/screen/deskripsi_produk.dart
+++ lib/view/toko/screen/deskripsi_produk.dart
@@ -217,7 +217,7 @@ class _DeskripsiState extends State<Deskripsi> {
             Positioned(
               left: 0,
               right: 0,
-              bottom: 0,
+              bottom: 10,
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: ReuseableButtonChat(
